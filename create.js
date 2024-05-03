2f68d570793f22a28b066a6444a931e57504efe3
alexaverill <alex.averill2013@gmail.com>
2024-05-02 19:57:30 -0500

    adding in some context for future work

---
+++ create.js
@@ -0,0 +1,16 @@
+//Select subset of base layers
+//  dialog with 4 base choise and manual of layers
+//Select bounds - optional can be set later
+// dialog then clickable bounds rect
+//Set zoom level - optional can be set later
+// show currnt bounds and text to suggest how
+//select point marker
+//  defaults plus custom icons
+// Set points
+//edit points
+//  title
+//  images
+//  local paths
+//  descrption
+// allso bounds and zoom level set
+//upload existing json
